c1615027499f1ba21f0fde220589acd8a3533966
Nastassia <nastassia.kl98@gmail.com>
2018-04-13 23:25:11 +0300

    Initial commit

---
+++ src/main/java/by/kalinklish/project/command/ActionFactory.java
@@ -0,0 +1,18 @@
+package by.kalinklish.project.command;
+
+import java.util.Optional;
+
+public class ActionFactory {
+    public static Optional<Command> defineCommand(String commandName) {
+
+        Optional<Command> current = Optional.empty();
+        if (commandName == null) {
+            return current;
+        }
+
+        CommandType type = CommandType.valueOf(commandName.toUpperCase());
+        current = Optional.of(type.getCommand());
+
+        return current;
+    }
+}
